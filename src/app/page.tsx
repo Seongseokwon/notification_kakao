7a0e8466b58b29e4aa067aee36188ea5ea23e309
SeongSeokwon <pam9411@naver.com>
2024-03-06 21:45:08 +0900

    Auth 오류 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
   useEffect(() => {
     setUserInfo(() => session?.user)
-    window.Kakao.Auth.setAccessToken(session!.user!.accessToken);
+    window.Kakao?.Auth.setAccessToken(session!.user!.accessToken);
   },[session])
   
   const selectFriends = async () => {
--- src/components/KakaoSDK.tsx
+++ src/components/KakaoSDK.tsx
@@ -10,14 +10,15 @@ type KakaoSDKProps = {
 const KakaoSDK = ({ children }: KakaoSDKProps) => {
   
   const kakaoInit = () => {
+  
     window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY!);
+
   }
   
   return (
     <>
      {children}
      <Script
-     async
       src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.0/kakao.min.js" 
       integrity="sha384-l+xbElFSnPZ2rOaPrU//2FF5B4LB8FiX5q4fXYTlfcG4PGpMkE1vcL7kNXI6Cci0" 
       crossOrigin="anonymous"
